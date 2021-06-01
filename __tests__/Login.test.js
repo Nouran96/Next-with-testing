3e010677b6cd17af3bc95b264718ffeb26caf1f6
Nouran Samy <nourans.96@yahoo.com>
2021-06-01 20:07:20 +0200

    Write test cases for login form

---
+++ __tests__/Login.test.js
@@ -0,0 +1,34 @@
+import { render, screen, fireEvent, waitFor } from "../test-utils";
+import Login from "../components/Login";
+
+describe("Login Form", () => {
+  it("checks for valid email field value with correct format", async () => {
+    render(<Login />);
+
+    fireEvent.change(screen.getByPlaceholderText("Email"), {
+      target: { value: "nouran@hotmail.com" },
+    });
+    fireEvent.click(screen.getByRole("button"));
+
+    const emailField = await waitFor(() => {
+      return screen.getByPlaceholderText("Email");
+    });
+
+    expect(emailField).toBeValid();
+  });
+
+  it("checks for valid password field value with minimum 8 characters", async () => {
+    render(<Login />);
+
+    fireEvent.change(screen.getByPlaceholderText("Password"), {
+      target: { value: "nouran123" },
+    });
+    fireEvent.click(screen.getByRole("button"));
+
+    const passwordField = await waitFor(() => {
+      return screen.getByPlaceholderText("Password");
+    });
+
+    expect(passwordField).toBeValid();
+  });
+});
--- tests/Login.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "../test-utils";
-import Login from "../components/Login";
-
-describe("Login", () => {
-  it("renders without crashing", () => {
-    render(<Login />);
-    expect(screen.getByRole("button", { name: "Login" })).toBeInTheDocument();
-  });
-});
